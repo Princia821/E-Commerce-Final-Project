153ab63b6d7068cb5fc800ea139d466a57916130
Princia <80551695+Princia821@users.noreply.github.com>
2022-12-05 16:55:27 +0000

    Update db_cred.php

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,8 +1,8 @@
 <?php
 
-define('SERVER', 'localhost');
-define('USER', 'root');
-define('DATABASE', 'letsheal5');
-define('PASSWORD', '');
+define('SERVER', 'us-cdbr-east-06.cleardb.net');
+define('USER', 'bb6cb50117e21f');
+define('DATABASE', 'heroku_d5ad2a82a356fed');
+define('PASSWORD', '8041a547');
 
 ?>
